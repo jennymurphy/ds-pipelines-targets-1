1a73d831b3340e50a59721fb60c6c014e354359a
Jennifer C Murphy <jmurphy@usgs.gov>
2022-04-21 11:54:56 -0500

    Created _targets.R

---
+++ _targets.R
@@ -0,0 +1,25 @@
+library(targets)
+source("code.R")
+tar_option_set(packages = c("tidyverse", "sbtools", "whisker"))
+list(
+  #get data
+  tar_target(model_RMSEs_csv,
+             getSBdata(childItemID = '5d925066e4b0c4f70d0d0599'),
+             format = "files"),
+  # prep data
+  tar_target(eval_data,
+             prepData_fun(savePath = "02_process/out/preppedData.rds"),
+             format = "file"),
+  # create plot
+  tar_target(fig1.png,
+             plotData_fun("fig1.png", "02_process/out/preppedData.rds"),
+             format = "file"),
+  # save processed data
+  targ_target(model_smry_results_csv,
+              write_csv(eval_data, file = "model_summary_results.csv"),
+              foramt = "file"),
+  # save model diags
+  tar_target(model_diagnostic_text_txt,
+             saveModDiags_fun("02_process/out/preppedData.rds"),
+             format = "file")
+)
